d85826384a1987d675e6b16674fbdc6aa44bda31
Prajna <prajnapprabhu3@gmail.com>
2023-04-04 23:26:09 +0530

    feat: server mongodb setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { connectDB } from './config/db';
+
 const app=express();
 
 app.get('/', (req,res)=>{
     res.send('home route running successfully');
 })
+
+connectDB();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server running on http://localhost:${process.env.PORT}`);
